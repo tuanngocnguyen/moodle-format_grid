13e132cb24f76a7ea961c34e66a1544433455189
Gareth J Barnard <gjbarnard@gmail.com>
2013-08-05 13:03:25 +0100

    V2.3.3

--- backup/moodle2/restore_format_grid_plugin.class.php
+++ backup/moodle2/restore_format_grid_plugin.class.php
@@ -56,7 +56,7 @@ class restore_format_grid_plugin extends restore_format_plugin {
 
         $data = (object) $data;
 
-		//print('Process Course');
+        //print('Process Course');
         //print_object($data);
 
         // We only process this information if the course we are restoring to
@@ -68,12 +68,11 @@ class restore_format_grid_plugin extends restore_format_plugin {
 
         $data->courseid = $this->task->get_courseid();
 
-		//print_object($data);  //  format_grid_summary.
+        //print_object($data);  //  format_grid_summary.
         if (!$DB->insert_record('format_grid_summary', $data)) {
             throw new moodle_exception('invalidrecordid', 'format_grid', '',
                 'Could not set summary status. Grid format database is not ready. An admin must visit the notifications section.');
         }
-		
 
         // No need to annotate anything here
     }
@@ -81,8 +80,8 @@ class restore_format_grid_plugin extends restore_format_plugin {
     protected function after_execute_structure() {
         
     }
-	
-	    /**
+
+    /**
      * Returns the paths to be handled by the plugin at section level
      */
     protected function define_section_plugin_structure() {
@@ -96,22 +95,22 @@ class restore_format_grid_plugin extends restore_format_plugin {
 
         return $paths; // And we return the interesting paths
     }
-	
+
     /**
      * Process the 'plugin_format_grid_section' element within the 'section' element in the 'section.xml' file in the '/sections/section_sectionid' folder
      * of the zipped backup 'mbz' file.
-	 * Discovered that the files are contained in the course repository with the new section number, so we just need to alter to the new value if any.
-	 * This was undertaken by performing a restore and using the url 'http://localhost/moodle23/pluginfile.php/94/course/section/162/mc_fs.png' where
-	 * I had an image called 'mc_fs.png' in section 1 which was id 129 but now 162 as the debug code told me.  '94' is just the context id.  The url was
-	 * originally created in '_make_block_icon_topics' of lib.php of the format.
-	 * Still need courseid in the 'format_grid_icon' table as it is used in discovering what records to remove when deleting a course, see lib.php 'format_grid_delete_course'.
+     * Discovered that the files are contained in the course repository with the new section number, so we just need to alter to the new value if any.
+     * This was undertaken by performing a restore and using the url 'http://localhost/moodle23/pluginfile.php/94/course/section/162/mc_fs.png' where
+     * I had an image called 'mc_fs.png' in section 1 which was id 129 but now 162 as the debug code told me.  '94' is just the context id.  The url was
+     * originally created in '_make_block_icon_topics' of lib.php of the format.
+     * Still need courseid in the 'format_grid_icon' table as it is used in discovering what records to remove when deleting a course, see lib.php 'format_grid_delete_course'.
      */
     public function process_gridsection($data) {
         global $DB;
 
         $data = (object) $data;
 
-		//print('Process Section '.$this->task->get_sectionid());
+        //print('Process Section '.$this->task->get_sectionid());
         //print_object($data);
 
         // We only process this information if the course we are restoring to
@@ -124,12 +123,21 @@ class restore_format_grid_plugin extends restore_format_plugin {
         $data->courseid = $this->task->get_courseid();
         $data->sectionid = $this->task->get_sectionid();
 
-        if (!$DB->insert_record('format_grid_icon', $data, true)) {
-            throw new moodle_exception('invalidrecordid', 'format_grid', '',
-                'Could not create icon. Grid format database is not ready. An admin must visit the notifications section.');
+        if (!$DB->record_exists('format_grid_icon', array('courseid' => $data->courseid, 'sectionid' => $data->sectionid))) {
+            if (!$DB->insert_record('format_grid_icon', $data, true)) {
+                throw new moodle_exception('invalidrecordid', 'format_grid', '',
+                    'Could not insert icon. Grid format table format_grid_icon is not ready. An administrator must visit the notifications section.');
+            }
+        } else {
+            $old = $DB->get_record('format_grid_icon', array('courseid' => $data->courseid, 'sectionid' => $data->sectionid));
+            $data->id = $old->id;
+            if (!$DB->update_record('format_grid_icon', $data)) {
+                throw new moodle_exception('invalidrecordid', 'format_grid', '',
+                    'Could not update icon. Grid format table format_grid_icon is not ready. An administrator must visit the notifications section.');
+            }
         }
 
         // No need to annotate anything here
-    }	
+    }
 
 }
