ae2abb8b2bc00a72d2d76c84b526a4343d69cdf2
Gareth J Barnard <gjbarnard@gmail.com>
2013-09-07 20:52:44 +0100

    CONTRIB-3240

--- yui/build/moodle-format_grid-gridkeys/moodle-format_grid-gridkeys-debug.js
+++ yui/build/moodle-format_grid-gridkeys/moodle-format_grid-gridkeys-debug.js
@@ -25,42 +25,45 @@ YUI.add('moodle-format_grid-gridkeys', function (Y, NAME) {
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+M.format_grid = M.format_grid || {};
 M.format_grid.gridkeys = M.format_grid.gridkeys || {};
-M.format_grid.gridkeys.init = function() {
-    Y().use('gallery-event-nav-keys', function(Y) {
-        Y.on('esc', function (e) {
-            e.preventDefault();
-            console.log("Esc pressed");
-            console.log("Selected section no: " + M.format_grid.selected_section_no);
-            M.format_grid.icon_toggle(e);
-        });
-        Y.on('left', function (e) {
-            e.preventDefault();
-            console.log("Left pressed");
-            M.format_grid.arrow_left(e);
-        });
-        Y.on('right', function (e) {
-            e.preventDefault();
-            console.log("Right pressed");
-            M.format_grid.arrow_right(e);
-        });
-        Y.on('enter', function (e) {
-            e.preventDefault();
-            console.log("Enter pressed");
-            console.log("Selected section no: " + M.format_grid.selected_section_no);
-            M.format_grid.icon_toggle(e);
-        });
-        Y.on('tab', function (e) {
-            e.preventDefault();
-            if (e.shiftKey) {
-                console.log("Shift Tab pressed");
+M.format_grid.gridkeys = {
+    init: function() {
+        Y().use('gallery-event-nav-keys', function(Y) {
+            Y.on('esc', function (e) {
+                e.preventDefault();
+                console.log("Esc pressed");
+                console.log("Selected section no: " + M.format_grid.selected_section_no);
+                M.format_grid.icon_toggle(e);
+            });
+            Y.on('left', function (e) {
+                e.preventDefault();
+                console.log("Left pressed");
                 M.format_grid.arrow_left(e);
-            } else {
-                console.log("Tab pressed");
+            });
+            Y.on('right', function (e) {
+                e.preventDefault();
+                console.log("Right pressed");
                 M.format_grid.arrow_right(e);
-            }
+            });
+            Y.on('enter', function (e) {
+            e.preventDefault();
+                console.log("Enter pressed");
+                console.log("Selected section no: " + M.format_grid.selected_section_no);
+                M.format_grid.icon_toggle(e);
+            });
+            Y.on('tab', function (e) {
+                e.preventDefault();
+                if (e.shiftKey) {
+                    console.log("Shift Tab pressed");
+                    M.format_grid.arrow_left(e);
+                } else {
+                console.log("Tab pressed");
+                    M.format_grid.arrow_right(e);
+                }
+            });
         });
-    });
+    }
 };
 
 
