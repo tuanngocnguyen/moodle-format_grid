3ce479619f4c6368a0e707eaad07881776e8d1f9
Gareth J Barnard <gjbarnard@gmail.com>
2013-10-07 21:11:15 +0100

    V2.5.4.4

--- db/upgrade.php
+++ db/upgrade.php
@@ -98,7 +98,7 @@ function xmldb_format_grid_upgrade($oldversion = 0) {
     }
 
     // Automatic 'Purge all caches'....
-    if ($oldversion < 2013100600) {
+    if ($oldversion < 2013100700) {
         purge_all_caches();
     }
 
