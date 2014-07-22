c53d08942708988b4ba63512de838bdd076088cb
Gareth J Barnard <gjbarnard@gmail.com>
2014-07-22 17:42:26 +0100

    Set specific gallery used by YUI module.

--- renderer.php
+++ renderer.php
@@ -174,7 +174,7 @@ class format_grid_renderer extends format_section_renderer_base {
             $course->numsections,
             json_encode($this->shadeboxshownarray)));
         // Initialise the key control functionality...
-        $PAGE->requires->yui_module('moodle-format_grid-gridkeys', 'M.format_grid.gridkeys.init', null, null, true);
+        $PAGE->requires->yui_module('moodle-format_grid-gridkeys', 'M.format_grid.gridkeys.init', null, '2011.02.02-21-07', true);
     }
 
     /**
